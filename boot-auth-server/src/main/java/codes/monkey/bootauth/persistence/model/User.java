a4ee1520ce1f0c26d05422153090b8c6567ec60d
Miguel A <miguelsc@gmail.com>
2017-10-04 12:27:45 +0200

    Auth-server: Showing user name in navigation bar.

--- boot-auth-server/src/main/java/codes/monkey/bootauth/persistence/model/User.java
+++ boot-auth-server/src/main/java/codes/monkey/bootauth/persistence/model/User.java
@@ -155,8 +155,9 @@ public class User implements Serializable{
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.append("User [id=").append(id).append(", firstName=").append(firstName).append(", lastName=").append(lastName).append(", email=").append(email).append(", password=").append(password).append(", enabled=").append(enabled).append(", isUsing2FA=")
-                .append(isUsing2FA).append(", roles=").append(roles).append("]");
+        /*builder.append("User [id=").append(id).append(", firstName=").append(firstName).append(", lastName=").append(lastName).append(", email=").append(email).append(", password=").append(password).append(", enabled=").append(enabled).append(", isUsing2FA=")
+                .append(isUsing2FA).append(", roles=").append(roles).append("]");*/
+        builder.append(firstName);
         return builder.toString();
     }
 
